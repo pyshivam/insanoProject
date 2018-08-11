c0522d1fcddbb63d93ac1cdac22971255d878cf9
Shivam Gupta <sg129718@gmail.com>
2018-08-12 00:28:45 +0530

    updated
    Signed-off-by: pyshivam <sg129718@gmail.com>

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,40 @@
-function move() {
-    let elem = document.getElementById("prog");
-    let width = 1;
-    let id = setInterval(frame, 100);
-    function frame() {
-        if (width >= 100) {
-            clearInterval(id);
-        } else {
-            width++;
-            elem.style.width = width + '%';
+let flag_t = false;
+let flag_d = false;
+
+function move(name) {
+
+    function work(elem, type) {
+        let width = 1;
+        let id = setInterval(frame, 100);
+
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                if (type === "drive") {
+                    flag_d = false;
+                } else if (type === "torrent") {
+                    flag_t = false;
+                }
+            } else {
+                width++;
+                elem.style.width = width + '%';
+            }
         }
     }
+
+    if (name === "drive") {
+        if (flag_d === false) {
+            var elem = document.getElementById("prog");
+            work(elem, "drive");
+            flag_d = true;
+        }
+    } else if (name === "torrent") {
+        if (flag_t === false) {
+            elem = document.getElementById("m-prog");
+            work(elem, "torrent");
+            flag_t = true;
+        }
+    }
+
+
 }
